fc9ffedbaff65657388aa34451d69cdc2a193f93
timkim <timk@adobe.com>
2021-11-18 10:51:07 -0800

    Don't pass in FASTLY_TOKEN as param

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const FastlyPurge = require('fastly-purge');
     const globber = await glob.create(patterns.join('\n'));
     
     // Wrap purge.url() into a Promise
-    const purgeURL = (url, FASTLY_TOKEN) => {
+    const purgeURL = (url) => {
       return new Promise((res, rej) => {
         purge.url(url, {apiKey: FASTLY_TOKEN}, (err, result) => {
           if (result) res(result);
